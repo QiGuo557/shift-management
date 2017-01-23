a1c773a0845635719ae44fc179f47fc60c725c04
Qi Guo <qi.guo557@gmail.com>
2017-01-22 21:21:30 -0600

    Added functionality for updating availability: Users are able to update availability through a pop-up window.
    Update and Add availability are using the same ajax

--- handlers.py
+++ handlers.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
 import os
-from datetime import datetime
+import json
+
 from google.appengine.ext.webapp import template
-from models import initialize_availability, insert_availability, retrieve_availability
+from models import time_interval_to_dict, update_availability
+
+
+def date_handler(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        raise TypeError
 
 
 class AddAvailabilityHandler(webapp2.RequestHandler):
+
     def get(self):
-        template_values = {}
         path = os.path.join(os.path.dirname(__file__), 'templates/available-time.html')
+
+        template_values = {
+            "time_interval": json.dumps(time_interval_to_dict('Qi', 1))
+        }
         self.response.write(template.render(path, template_values))
 
     def post(self):
-        date = datetime.strptime(self.request.get('availability_date'), '%Y-%m-%d').date()
-        month = date.month
-        start_time = datetime.strptime(self.request.get('availability_start_time'), '%H:%M').time()
-        end_time = datetime.strptime(self.request.get('availability_end_time'), '%H:%M').time()
-        insert_availability(employee="Qi", month=month, date=date, start_time=start_time, end_time=end_time)
-        retrieve_availability(employee="Qi", month=month)
-        self.redirect('/')
+        time_intervals = json.loads(self.request.get('time_interval_list'))
+        month = int(self.request.get('month'))
+        update_availability('Qi', month, time_intervals)
+
+        template_values = time_interval_to_dict('Qi', int(month))
+
+        self.response.write(json.dumps(template_values))
+
 
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from google.appengine.ext import ndb
 
 
@@ -5,6 +6,13 @@ class TimeInterval(ndb.Model):
     date = ndb.DateProperty()
     start_time = ndb.TimeProperty()
     end_time = ndb.TimeProperty()
+
+    def to_dict(self):
+        time_interval_dict = {}
+        time_interval_dict['date'] = self.date.isoformat()
+        time_interval_dict['start_time'] = self.start_time.isoformat()
+        time_interval_dict['end_time'] = self.end_time.isoformat()
+        return time_interval_dict
 
 
 class Availability(ndb.Model):
@@ -21,9 +29,25 @@ class Availability(ndb.Model):
         self.key = self.build_key(self.employee, self.month)
 
     @classmethod
-    def query_availability(cls, employee, month):
+    def query_availability_by_key(cls, employee, month):
         availability_key = cls.build_key(employee, month)
-        return cls.query(Availability.key == availability_key).get()
+        return cls.query(Availability.key == availability_key).order(cls.time_interval.date,
+                                                                     cls.time_interval.start_time).get()
+
+
+def time_interval_to_dict(employee, month):
+    availability = retrieve_availability(employee, month)
+    if availability:
+        time_interval_list = availability.time_interval
+
+        template_values = {
+            "time_interval_list": [time_interval.to_dict() for time_interval in time_interval_list]
+        }
+    else:
+        template_values = {
+            "time_interval_list": []
+        }
+    return template_values
 
 
 def initialize_availability(employee, month):
@@ -45,5 +69,24 @@ def insert_availability(employee, month, date, start_time, end_time):
     return availability
 
 
+def update_availability(employee, month, time_interval_list):
+    availability = retrieve_availability(employee, int(month))
+    time_intervals = []
+
+    for time_interval in time_interval_list:
+        date = datetime.strptime(time_interval['date'], '%Y-%m-%d').date()
+        start_time = datetime.strptime(time_interval['start_time'], '%H:%M:%S').time()
+        end_time = datetime.strptime(time_interval['end_time'], '%H:%M:%S').time()
+        time_intervals.append(TimeInterval(date=date, start_time=start_time, end_time=end_time))
+
+        if not availability:
+            availability = Availability(employee=employee, month=month)
+            availability.set_key()
+
+    availability.time_interval = time_intervals
+    availability.put()
+    return availability
+
+
 def retrieve_availability(employee, month):
-    return Availability.query_availability(employee, month)
+    return Availability.query_availability_by_key(employee, month)
